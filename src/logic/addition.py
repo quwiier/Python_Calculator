253843b03af43882855010e52ce5798accde648e
Daniil Telegin <daniil.collis@gmail.com>
2024-07-11 21:39:16 +0300

    almost all buttons have been added, logic is now released using the built-in method evaluate()

--- src/logic/addition.py
+++
@@ -1,8 +0,0 @@
-class Addiction:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def add(term1: int | float, term2: int | float) -> int | float:
-        return term1 + term2
-
--- src/logic/subtraction.py
+++
@@ -1,7 +0,0 @@
-class Subtraction:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def sub(term1: int | float, term2: int | float) -> int | float:
-        return term1 - term2
